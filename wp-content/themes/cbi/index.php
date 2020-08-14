e6739bdb3ef3985214543bc0326c518db175c4d9
Dan Phiffer <dan@phiffer.org>
2020-08-13 20:57:02 -0400

    add index.php

---
+++ wp-content/themes/cbi/index.php
@@ -0,0 +1,50 @@
+<?php
+
+get_header();
+
+global $sections;
+$sections = array();
+
+?>
+<section>
+	<div class="container">
+		<h2 class="page-title"><?php the_title(); ?></h2>
+		<div class="page-content larger">
+			<?php the_content(); ?>
+		</div>
+	</div>
+</section>
+<section class="tabs resize-bg">
+	<nav>
+		<a href="#collective" class="selected">Our Collective</a>
+		<a href="#story">Our Story</a>
+		<a href="#board">Our Board</a>
+	</nav>
+	<div class="container">
+		<?php
+
+		foreach ($sections as $section => $blocks) {
+
+			$selected = ($section == 'collective') ? ' selected' : '';
+
+			echo "<div id=\"tab-$section\" class=\"tab$selected\">\n";
+
+			foreach ($blocks as $block) {
+				if ($block['name'] == 'acf/about-person') {
+					include __DIR__ . '/block-about-person.php';
+				} else if ($block['name'] == 'acf/about-text') {
+					include __DIR__ . '/block-about-text.php';
+				}
+			}
+
+			echo "</div>\n";
+
+		}
+
+		?>
+	</div>
+	<div class="image-credit">Website Artwork by <a href="https://www.minamjafari.com/art">Mina M. Jafari</a></div>
+</section>
+<?php
+
+get_footer();
