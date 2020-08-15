b9cac0833d1bbf79248936de0d572b8739d57044
Dan Phiffer <dan@phiffer.org>
2020-08-15 16:35:42 -0400

    social media links

--- wp-content/themes/cbi/footer.php
+++ wp-content/themes/cbi/footer.php
@@ -4,9 +4,9 @@
 						<a href="/" class="identity"><img src="<?php asset_url('img/logotype-white.png'); ?>" alt="Collective for Black Iranians"></a>
 						<ul>
 							<li class="label">Follow Us</li>
-							<li class="social twitter"><a href="#"><i class="fab fa-twitter"></i></a></li>
-							<li class="social instagram"><a href="#"><i class="fab fa-instagram"></i></a></li>
-							<li class="social vimeo"><a href="#"><i class="fab fa-vimeo"></i></a></li>
+							<li class="social twitter"><a href="https://twitter.com/BlackIranians"><i class="fab fa-twitter"></i></a></li>
+							<li class="social instagram"><a href="https://www.instagram.com/collectiveforblackiranians"><i class="fab fa-instagram"></i></a></li>
+							<!-- <li class="social vimeo"><a href="#"><i class="fab fa-vimeo"></i></a></li>-->
 						</ul>
 					</nav>
 					<div class="bottom">
